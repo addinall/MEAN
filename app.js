e4dc88e819615092b5fc338982c3902cd42f20e6
addinall <addinall@addinall.net>
2015-10-02 09:49:51 +1000

    RETAB

--- app.js
+++ app.js
@@ -8,16 +8,16 @@
 //                that will hopefully give me the basis for my new
 //                RESTful stack under development.  I was going to
 //                use angular with a set of PHP REST APIs, I still
-//		          might, but this needs a try out as well.
-//		          I like the idea of using Javascript all through the
-//		          stack for the front, mid and back end, and always
-//		          using JSON as the data package WITHOUT pesky add-on
-//		          libaries.  I tried the mean.io stack but like
-//		          most things written by "bubbly, young and dynamic"
-//		          coders it was BIG, FAY, UGLY and CRAP.
-//		          Soooo, as usual, roll my own in a 100th of the time it
-//		          takes just to install and look after that new-age
-//		          bollocks.
+//                might, but this needs a try out as well.
+//                I like the idea of using Javascript all through the
+//                stack for the front, mid and back end, and always
+//                using JSON as the data package WITHOUT pesky add-on
+//                libaries.  I tried the mean.io stack but like
+//                most things written by "bubbly, young and dynamic"
+//                coders it was BIG, FAT, UGLY and CRAP.
+//                Soooo, as usual, roll my own in a 100th of the time it
+//                takes just to install and look after that new-age
+//                bollocks.
 
 
 
