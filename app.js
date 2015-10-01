606a93cf4982ceb9c25565a89a3cef5cd05591cb
addinall <addinall@addinall.net>
2015-10-02 07:14:02 +1000

    First commit after git creation 0715

---
+++ app.js
@@ -0,0 +1,21 @@
+// CAPTAIN SLOG
+// vim: set expandtab tabstop=4 shiftwidth=4 autoindent smartindent:
+// File         : index.html
+// System       : mean-app
+// Date         : Oct 2nd 2015
+// Author       : Mark Addinall
+// Synopsis     : A little play with using a MEAN application stack
+//                that will hopefully give me the basis for my new
+//                RESTful stack under development.  I was going to
+//                use angular with a set of PHP REST APIs, I still
+//		          might, but this needs a try out as well.
+//		          I like the idea of using Javascript all through the
+//		          stack for the front, mid and back end, and always
+//		          using JSON as the data package WITHOUT pesky add-on
+//		          libaries.  I tried the mean.io stack but like
+//		          most things written by "bubbly, young and dynamic"
+//		          coders it was BIG, FAY, UGLY and CRAP.
+
+
+
+
