231bf2ac3c2b41bd63c6ed4a559758e46771c391
addinall <addinall@addinall.net>
2015-10-11 07:43:37 +1000

    BIG changes. Bolting on Mongo and NodeJS and stuff

--- app.js
+++ app.js
@@ -1,113 +1,60 @@
-// CAPTAIN SLOG
-// vim: set expandtab tabstop=4 shiftwidth=4 autoindent smartindent:
-// File         : app.js 
-// System       : mean-app
-// Date         : Oct 2nd 2015
-// Author       : Mark Addinall
-// Synopsis     : A little play with using a MEAN application stack
-//                that will hopefully give me the basis for my new
-//                RESTful stack under development.  I was going to
-//                use angular with a set of PHP REST APIs, I still
-//                might, but this needs a try out as well.
-//                I like the idea of using Javascript all through the
-//                stack for the front, mid and back end, and always
-//                using JSON as the data package WITHOUT pesky add-on
-//                libaries.  I tried the mean.io stack but like
-//                most things written by "bubbly, young and dynamic"
-//                coders it was BIG, FAT, UGLY and CRAP.
-//                Soooo, as usual, roll my own.
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 
-
-//--------------------------------------
-var app = angular.module('meanNews', ['ui.router']);    // this is named in the html declaration,
-                                                        // encapsulates our entire system
-
-
-//----------
-app.config([
-
-    // we are using ui-router so we need to configure it.
-    // we will use the angular method config to set up
-    // a home state.
-
-    '$stateProvider',
-    'urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
-
-        $stateProvider
-            .state('home', {                            // these next few variables will
-                url: '/home',                           // obvioisly change to reflect the
-                templateUrl: '/home.html',              // home state of the target machine
-                controller: 'mainControl'               // environment
-        });
-
-        $url(RouterProvider.otherwise('home');          // if we don't get a valid url, go HOME
-}]);
-
-
-
-//--------------------------------
-app.factory('items', [function() {
-
-    // I wrote the main controllers first and was then to find out
-    // that the use of "factories" or "services" are considered best
-    // practice in this type of architecture.  Dunno.  To me it seems to
-    // add unwarrented complexity to what was a rather elegant and
-    // simple to use tool.  However, being new to this I shall explore it.
-    // If it is just another RoR, SCALA, Haskell wank I'll chuck it away.
-    
-    var newsItems = {                               // what is going on here? we are creating a
-        items: []                                   // new Javascript object newsItems, that
-    };                                              // has an array property, items.  This just lifts
-    return newsItems;                               // the scope of this array OUT of our main controller
-                                                    // OK.  I know what is going on. 'factories' and
-                                                    // 'services' are a couple of trendy ways of describing
-                                                    // a chunk-o-GLOBAL memory containing an object
-                                                    // encapsulating methods and properties that might
-                                                    // be of interest elsewhere.
-}])                                                 // Sorta OODish.  Functional guys will have a fit!
-
-
-
-//-----------------------------
-app.controller('mainControl', [                     // just the one controller for now
-    '$scope',                                       // add a second controller 'factory'
-    'items',                                        // inject the object into this controller
-    function($scope, items) {                       // scope is the particular area of memory            
-        $scope.header = "MEAN News BLOG!";          // Angular uses for the bi-directional
-        $scope.news = items.items;                  // bind scope to the object injected from factory
-                                                    // binding of variables in this new model.
-                                                    // it works rather well for a change.
-                                                    // news is our stack of news items
-    
-        
-        //---------------------------
-        $scope.addNews = function() {
-
-            // add a method to input new bloggies
-            // these functions should almost certainly
-            // be in the items factory.
-            // I'll change it later after I get
-            // routing sorted out.
-    
-            if (!$scope.title || $scope.title === '') {
-                return;                              // don't need empty news items
-            }
-
-            $scope.news.push({  title: $scope.title, 
-                                link: $scope.link,
-                                votes: 0});          // create new news, no votes
-            $scope.title = '';                       // and empty that var for next time
-            $scope.link  = '';                       // ditto
-        };
-
-        //--------------------------------------
-        $scope.incrementLikes = function(news) {
-
-            // add a LIKE button, fancy up the graphics later
-            
-            news.votes += 1;
-        };
-}]);
-
+module.exports = app;
---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,152 @@
+// CAPTAIN SLOG
+// vim: set expandtab tabstop=4 shiftwidth=4 autoindent smartindent:
+// File         : app.js 
+// System       : mean-app
+// Date         : Oct 2nd 2015
+// Author       : Mark Addinall
+// Synopsis     : A little play with using a MEAN application stack
+//                that will hopefully give me the basis for my new
+//                RESTful stack under development.  I was going to
+//                use angular with a set of PHP REST APIs, I still
+//                might, but this needs a try out as well.
+//                I like the idea of using Javascript all through the
+//                stack for the front, mid and back end, and always
+//                using JSON as the data package WITHOUT pesky add-on
+//                libaries.  I tried the mean.io stack but like
+//                most things written by "bubbly, young and dynamic"
+//                coders it was BIG, FAT, UGLY and CRAP.
+//                I put this together from various tutorials and coding BLOGs. 
+
+
+
+//--------------------------------------
+var app = angular.module('meanNews', ['ui.router']);    // this is named in the html declaration,
+                                                        // encapsulates our entire system
+
+
+//----------
+app.config([
+
+    // we are using ui-router so we need to configure it.
+    // we will use the angular method config to set up
+    // a home state.
+    //
+    // This is the MEAN way of doing multi function apps
+    // in a single page.  The particular HTML/CSS to be
+    // actioned is described in a 'template' with a unique
+    // id that reflect the HTML naming convention in semi-traditional
+    // web app builds.
+    //
+    // Of course, I was doing this a decade ago.  The wurld
+    // caught up with me yet again!
+    // http://www.flsc.com.au/
+
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('home', {                            // these next few variables will
+                url: '/home',                           // obvioisly change to reflect the
+                templateUrl: '/home.html',              // home state of the target machine
+                controller: 'mainControl'               // environment
+        });
+
+
+        $stateProvider                                  // OK.  now the home is working we
+            .state('posts', {                           // will introduce a postings 'page'.
+                url: '/posts{id}',                      // {id} is bracketed because it is
+                templateUrl: '/posts.html',             // a route parameter we will feed
+                controller: 'postControl'               // to the controller.  This is part
+        });                                             // of the CRUD/REST strategy
+
+        $urlRouterProvider.otherwise('home');           // if we don't get a valid url, go HOME
+}]);
+
+
+
+//--------------------------------
+app.factory('items', [function() {
+
+    // I wrote the main controllers first and was then to find out
+    // that the use of "factories" or "services" are considered best
+    // practice in this type of architecture.  Dunno.  To me it seems to
+    // add unwarrented complexity to what was a rather elegant and
+    // simple to use tool.  However, being new to this I shall explore it.
+    // If it is just another RoR, SCALA, Haskell wank I'll chuck it away.
+    
+    var newsItems = {                               // what is going on here? we are creating a
+        items: []                                   // new Javascript object newsItems, that
+    };                                              // has an array property, items.  This just lifts
+    return newsItems;                               // the scope of this array OUT of our main controller
+                                                    // OK.  I know what is going on. 'factories' and
+                                                    // 'services' are a couple of trendy ways of describing
+                                                    // a chunk-o-GLOBAL memory containing an object
+                                                    // encapsulating methods and properties that might
+                                                    // be of interest elsewhere.
+}])                                                 // Sorta OODish.  Functional guys will have a fit!
+
+
+
+//-----------------------------
+app.controller('mainControl', [                     // just the one controller for now
+    '$scope',                                       // add a second controller 'factory'
+    'items',                                        // inject the object into this controller
+    function($scope, items) {                       // scope is the particular area of memory            
+        $scope.header = "MEAN News BLOG!";          // Angular uses for the bi-directional
+        $scope.news = items.items;                  // bind scope to the object injected from factory
+                                                    // binding of variables in this new model.
+                                                    // it works rather well for a change.
+                                                    // news is our stack of news items
+    
+        
+        //---------------------------
+        $scope.addNews = function() {
+
+            // add a method to input new bloggies
+            // these functions should almost certainly
+            // be in the items factory.
+            // I'll change it later after I get
+            // routing sorted out.
+    
+            if (!$scope.title || $scope.title === '') {
+                return;                              // don't need empty news items
+            }
+
+            $scope.news.push({  title: $scope.title, 
+                                link: $scope.link,
+                                votes: 0,
+                                comments: [
+                                    {author: 'Addinall', body: 'Nice comments!', votes: 0},
+                                    {author: 'DDuck', body: 'QUACK QUACK!',votes: 0}
+                                ]
+            });                                      // create new news, no votes
+            $scope.title = '';                       // and empty that var for next time
+            $scope.link  = '';                       // ditto
+        };
+
+        //--------------------------------------
+        $scope.incrementLikes = function(news) {
+
+            // add a LIKE button, fancy up the graphics later
+            
+            news.votes += 1;
+        };
+}]);
+
+
+//-----------------------------
+app.controller('postControl', [
+
+    // now for a controller that will take care of our 
+    // view posting page
+
+    '$scope',
+    '$stateParams',
+    '$posts',
+    function($scope, $stateParams, posts) {
+        $scope.post = items.items[$stateParams.id];         // target one individua; post of a news item list
+
+}]);
+
+
